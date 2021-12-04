685b3a65186c5fd84764094ec9f3d01076a7a891
coderRamL <89924197+coderRamL@users.noreply.github.com>
2021-12-04 16:56:34 -0500

    Update Account.java

--- src/Account.java
+++ src/Account.java
@@ -7,7 +7,9 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
-
+/* 
+ * Represents the Account of a Teacher or a Student that gives access to certain options based on what type of account the person has 
+ */
 public class Account {
     //The username of the person's account
     private String username;
@@ -16,13 +18,23 @@ public class Account {
     //Determines if the person is a student or not
     private boolean isStudent;
     //Represents the account id number of the person
-
+    
+    /*
+     * Constructs a newly allocated object Account object with the specified username, password, and student status
+     * @param username The username to be used in construction
+     * @param password The password to be used in construction
+     * @param isStudent The student status to be used in construction
+     */
     public Account(String username, String password, boolean isStudent) {
         this.username = username;
         this.password = password;
         this.isStudent = isStudent;
     }
-
+    
+    /*
+     * Creates a Course object based on the name of the course
+     * @return Returns a Course object with the specified name of the course
+     */
     public Course createCourse(Scanner s) {
         /* GUI version 
         name = JOptionPane.showInputDialog(null, "Enter the name of the course", "Course Name", JOptionPane.QUESTION_MESSAGE);
@@ -31,7 +43,11 @@ public class Account {
         System.out.println("Enter the name of the course");
         return new Course(s.nextLine());
     }
-
+    
+    /* 
+     * Creates a quiz with a specified amount of questions with four answer choices in each question
+     * @return Returns a Quiz object that represents a quiz with a certain name that has a certain amount of questions
+     */
     public Quiz createQuiz(Scanner s) {
         /* GUI version 
         JOptionPane.showInputDialog(null, "Enter the Name of the Quiz", "Quiz Name", JOptionPane.QUESTION_MESSAGE);
@@ -74,7 +90,11 @@ public class Account {
         
         return new Quiz(name, q);
     }
-
+    
+    /* 
+     * Modifies and changes the name and questions of the specified quiz identified by setting a new name and list of questions
+     * @param quiz The specified quiz that is being modified and changed
+     */
     public void editQuiz(Scanner s, Quiz quiz) {
         /* GUI version
         String name = JOptionPane.showInputDialog(null, "Enter the new Name of the Quiz", "New Quiz Name", JOptionPane.QUESTION_MESSAGE);
@@ -115,7 +135,12 @@ public class Account {
         System.out.println("Edited Successfully");
         quiz.setQuestions(q);
     }
-
+    
+    /*
+     * Grades the student's submission for each individual question, calculates the total score for the student, and returns that student's submission
+     * @param sub The student's submission that is being graded
+     * @return The Submission object that represents the student's submission that was being graded
+     */
     public Submission gradeSubmission(Scanner s, Submission sub) {
         /* GUI version
         int[] subScore = new int[sub.getAnswers().length];
@@ -152,7 +177,13 @@ public class Account {
         sub.setTotalGrades(total);
         return sub;
     }
-
+    
+    /* 
+     * Grades a student's submission based on the file path indicated
+     * A score is given per line (per question) and a total score is calulcated with the submission being returned after it is graded
+     * @param sub The student's submission that is being graded
+     * @return The Submission object that represents the student's submission that was being graded, if it cannot return the submission then it returns null
+     */
     public Submission gradeSubmissionViaFile(Scanner s, Submission sub) {
         /* GUI version
         for (int i = 0; i < sub.getAnswers().length; i++) {
@@ -221,7 +252,12 @@ public class Account {
         }
         return null;
     }
-
+    
+    /* 
+     * Student answers the questions on the quiz and submits it
+     * @param q The quiz that the student is taking
+     * @return Returns the Submission object that represents the student's quiz submission
+     */
     public Submission takeQuiz(Scanner s, Quiz q) {
         /* GUI version
         JOptionPane.showMessageDialog(null, "You are now taking Quiz " + q.getName(), "Take Quiz", JOptionPane.INFORMATION_MESSAGE);
@@ -243,7 +279,13 @@ public class Account {
         System.out.println("Finished taking the quiz");
         return new Submission(this, answers, q);
     }
-
+    
+    /* 
+     * Student takes the quiz based on the file path they enter
+     * The student enters an answer on each line and submits their answers when done
+     * @param q The quiz that the student is taking
+     * @return Returns the Submission object that represents the student's submission if the submission was a success, otherwise it returns null
+     */
     public Submission takeQuizViaFile(Scanner s, Quiz q) {
         /* GUI version
         int[] answers = new int[q.getQuestions().length];
@@ -296,23 +338,43 @@ public class Account {
         }
         return null;
     }
-
+    
+    /* 
+     * Returns the specified password
+     * @return Returns the specified password
+     */ 
     public String getPassword() {
         return password;
     }
-
+    
+    /* 
+     * Returns the specified student status
+     * @return Returns the specified student status
+     */ 
     public boolean isStudent() {
         return isStudent;
     }
-
+    
+    /* 
+     * Returns the specified username
+     * @return Returns the specified username
+     */ 
     public String getUsername() {
         return username;
     }
-
+    
+    /* 
+     * Updates the password of this account with the specified password
+     * @param password The password to be used in the update
+     */
     public void setPassword(String password) {
         this.password = password;
     }
-
+    
+    /* 
+     * Updates the username of this account with the specified username
+     * @param username The username to be used in the update
+     */
     public void setUsername(String username) {
         this.username = username;
     }
