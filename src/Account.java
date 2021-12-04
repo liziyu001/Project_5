be3fb15421be5dbe8a1c5957ac2a3d31d929816e
coderRamL <89924197+coderRamL@users.noreply.github.com>
2021-12-04 17:15:52 -0500

    Update Account.java

--- src/Account.java
+++ src/Account.java
@@ -7,7 +7,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
-/* 
+/** 
  * Represents the Account of a Teacher or a Student that gives access to certain options based on what type of account the person has 
  */
 public class Account {
@@ -19,7 +19,7 @@ public class Account {
     private boolean isStudent;
     //Represents the account id number of the person
     
-    /*
+    /**
      * Constructs a newly allocated object Account object with the specified username, password, and student status
      * @param username The username to be used in construction
      * @param password The password to be used in construction
@@ -31,7 +31,7 @@ public class Account {
         this.isStudent = isStudent;
     }
     
-    /*
+    /**
      * Creates a Course object based on the name of the course
      * @return Returns a Course object with the specified name of the course
      */
@@ -44,7 +44,7 @@ public class Account {
         return new Course(s.nextLine());
     }
     
-    /* 
+    /** 
      * Creates a quiz with a specified amount of questions with four answer choices in each question
      * @return Returns a Quiz object that represents a quiz with a certain name that has a certain amount of questions
      */
@@ -91,7 +91,7 @@ public class Account {
         return new Quiz(name, q);
     }
     
-    /* 
+    /** 
      * Modifies and changes the name and questions of the specified quiz identified by setting a new name and list of questions
      * @param quiz The specified quiz that is being modified and changed
      */
@@ -136,7 +136,7 @@ public class Account {
         quiz.setQuestions(q);
     }
     
-    /*
+    /**
      * Grades the student's submission for each individual question, calculates the total score for the student, and returns that student's submission
      * @param sub The student's submission that is being graded
      * @return The Submission object that represents the student's submission that was being graded
@@ -178,7 +178,7 @@ public class Account {
         return sub;
     }
     
-    /* 
+    /** 
      * Grades a student's submission based on the file path indicated
      * A score is given per line (per question) and a total score is calulcated with the submission being returned after it is graded
      * @param sub The student's submission that is being graded
@@ -253,7 +253,7 @@ public class Account {
         return null;
     }
     
-    /* 
+    /** 
      * Student answers the questions on the quiz and submits it
      * @param q The quiz that the student is taking
      * @return Returns the Submission object that represents the student's quiz submission
@@ -280,7 +280,7 @@ public class Account {
         return new Submission(this, answers, q);
     }
     
-    /* 
+    /** 
      * Student takes the quiz based on the file path they enter
      * The student enters an answer on each line and submits their answers when done
      * @param q The quiz that the student is taking
@@ -339,7 +339,7 @@ public class Account {
         return null;
     }
     
-    /* 
+    /** 
      * Returns the specified password
      * @return Returns the specified password
      */ 
@@ -347,7 +347,7 @@ public class Account {
         return password;
     }
     
-    /* 
+    /** 
      * Returns the specified student status
      * @return Returns the specified student status
      */ 
@@ -355,7 +355,7 @@ public class Account {
         return isStudent;
     }
     
-    /* 
+    /** 
      * Returns the specified username
      * @return Returns the specified username
      */ 
@@ -363,7 +363,7 @@ public class Account {
         return username;
     }
     
-    /* 
+    /** 
      * Updates the password of this account with the specified password
      * @param password The password to be used in the update
      */
@@ -371,7 +371,7 @@ public class Account {
         this.password = password;
     }
     
-    /* 
+    /** 
      * Updates the username of this account with the specified username
      * @param username The username to be used in the update
      */
