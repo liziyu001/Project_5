b9ba6d82bc5e56074c12ad71c6199a291d5f12e3
coderRamL <89924197+coderRamL@users.noreply.github.com>
2021-12-09 00:27:36 -0500

    Update CourseMenu.java

--- src/CourseMenu.java
+++ src/CourseMenu.java
@@ -1,21 +1,35 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.util.ArrayList;
-
-
+/**
+ * Represents the course menu that displays all the quiz options
+ * @author Ram, William, Leo, Manas, Miras
+ * @version December 13, 2021
+ */
 public class CourseMenu extends javax.swing.JFrame {
-
+    //Button that allows teacher to create a quiz
     private javax.swing.JButton createButton;
+    //Button that allows teacher to delete a quiz
     private javax.swing.JButton deleteButton;
+    //Button that allows teacher to edit a quiz
     private javax.swing.JButton editButton;
+    //Button that allows teacher to go back
     private javax.swing.JButton backButton;
+    
+    /**
+     * Constructs a newly allocated CourseMenu object that calls the initComponents() method
+     */
     public CourseMenu() {
         initComponents();
     }
-
+    
+    /**
+     * Creates and initializes the four buttons with the set text and the action listeners, which performs the functionality of each button when clicked
+     */
     private void initComponents() {
 
         createButton = new javax.swing.JButton();
@@ -92,7 +106,12 @@ public class CourseMenu extends javax.swing.JFrame {
         panel.add(backButton);
         getContentPane().add(panel, BorderLayout.CENTER);
     }
-
+    
+    /**
+     * @Description action when create button is pressed, represents creating a quiz
+     * @Param [evt] Allows the button's action to be performed
+     * @return void
+     */
     private void createButtonActionPerformed(java.awt.event.ActionEvent evt) {
         int choice = JOptionPane.showConfirmDialog(null, "Do you want to create by file import?", "File import",JOptionPane.YES_NO_OPTION);
         ArrayList<String> in = new ArrayList<String>();
@@ -159,7 +178,12 @@ public class CourseMenu extends javax.swing.JFrame {
             JOptionPane.showMessageDialog(null, "Error", "Fail", JOptionPane.ERROR_MESSAGE);
         }
     }
-
+    
+    /**
+     * @Description action when edit button is pressed, represents editing a quiz
+     * @Param [evt] Allows the button's action to be performed
+     * @return void
+     */
     private void editButtonActionPerformed(java.awt.event.ActionEvent evt) {
         ArrayList<String> c = new ArrayList<String>();
         c.add(Main.getCurrentCourse());
@@ -182,35 +206,35 @@ public class CourseMenu extends javax.swing.JFrame {
 //                "How many Question do you want to include",
 //                "Create Quiz", JOptionPane.QUESTION_MESSAGE);
 //        in.add(n);
-        while(true){
+        while(true) {
             String numbOfQuestions = JOptionPane.showInputDialog(null, "Enter new amount of questions", "Edit Quiz", JOptionPane.QUESTION_MESSAGE);
-            if (numbOfQuestions == null){
+            if (numbOfQuestions == null) {
                 break;
             }
-            else{
-                try{
+            else {
+                try {
                     in.add(numbOfQuestions);
                     int numberOfQuestions = Integer.parseInt(numbOfQuestions);
-                    for (int i = 0; i<numberOfQuestions; i++){
+                    for (int i = 0; i < numberOfQuestions; i++) {
                         String questionPrompt = JOptionPane.showInputDialog(null, "Enter new question Prompt:", "Edit Quiz", JOptionPane.QUESTION_MESSAGE);
-                        if (questionPrompt == null){
+                        if (questionPrompt == null) {
                             break;
                         }
-                        else if (questionPrompt.contains(";")){
+                        else if (questionPrompt.contains(";")) {
                             JOptionPane.showMessageDialog(null, "Value cannot include a semicolon", "Error", JOptionPane.ERROR_MESSAGE);
                         }
-                        else{
+                        else {
                             in.add(questionPrompt);
                         }
-                        for (int j=0; j<4; j++){
+                        for (int j = 0; j < 4; j++) {
                             String answerChoice = (String) JOptionPane.showInputDialog(null, "Enter " +(j+1)+" choice of the Question:", "Edit Questions", JOptionPane.QUESTION_MESSAGE);
-                            if(answerChoice == null){
+                            if (answerChoice == null) {
                                 break;
                             }
-                            else if(answerChoice.contains(";")){
+                            else if (answerChoice.contains(";")) {
                                 JOptionPane.showMessageDialog(null, "Value cannot include a semicolon", "Error", JOptionPane.ERROR_MESSAGE);
                             }
-                            else{
+                            else { 
                                 in.add(answerChoice);
                             }
                         }
@@ -222,7 +246,7 @@ public class CourseMenu extends javax.swing.JFrame {
                         JOptionPane.showMessageDialog(null, "Error", "Fail", JOptionPane.ERROR_MESSAGE);
                     }
                     break;
-                }catch (Exception ex){
+                } catch (Exception ex) {
                     JOptionPane.showMessageDialog(null, "Please enter the String", "Erorr", JOptionPane.ERROR_MESSAGE);
                     break;
                 }
@@ -252,7 +276,12 @@ public class CourseMenu extends javax.swing.JFrame {
 //            JOptionPane.showMessageDialog(null, "Error", "Fail", JOptionPane.ERROR_MESSAGE);
 //        }
     }
-
+    
+    /**
+     * @Description action when delete button is pressed, represents deleting a quiz
+     * @Param [evt] Allows the button's action to be performed
+     * @return void
+     */
     private void deleteButtonActionPerformed(java.awt.event.ActionEvent evt) {
         ArrayList<String> c = new ArrayList<String>();
         c.add(Main.getCurrentCourse());
@@ -275,12 +304,20 @@ public class CourseMenu extends javax.swing.JFrame {
         }
     }
 
-
+    /**
+     * @Description action when back button is pressed
+     * @Param [evt] Allows the button's action to be performed
+     * @return void
+     */
     private void backButtonActionPerformed(java.awt.event.ActionEvent evt) {
         this.setVisible(false);
         new TeacherMenu().setVisible(true);
     }
-
+    
+    /**
+     * Main method that invokes the AWT and runs the CourseMenu object that displays the buttons and all its functionalities
+     * @param args Stores the command line arguments
+     */
     public static void main(String args[]) {
 
         SwingUtilities.invokeLater(new Runnable() {
