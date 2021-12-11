83e77fee2ef2d6577d7ad793c9215ad4173f7b43
coderRamL <89924197+coderRamL@users.noreply.github.com>
2021-12-11 17:32:16 -0500

    Update StudentMenu.java

--- src/StudentMenu.java
+++ src/StudentMenu.java
@@ -211,7 +211,7 @@ public class StudentMenu extends javax.swing.JFrame {
     }
 
     /**
-     * Main method that invokes the AWT and runs the StudentMenu object that displays the buttons and all its functionalities
+     * Main method that invokes run using EDT and runs the StudentMenu object that displays the buttons and all its functionalities
      *
      * @param args Stores the command line arguments
      */
